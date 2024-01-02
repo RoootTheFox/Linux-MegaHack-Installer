c772171925f8bef71ff5600eb72e26ae2ad4f469
rooot <hey@rooot.gay>
2024-01-02 20:22:29 +0100

    chore: consistent indentation
    Signed-off-by: rooot <hey@rooot.gay>

--- megahack_installer.sh
+++ megahack_installer.sh
@@ -14,21 +14,20 @@ BOLD_YELLOW="\033[1;33m"
 
 # logging functions
 error() {
-	if [ "$2" == "1" ]; then
-		printf "        ${RED}%s${RESET}\n" "$1"
-	else
-		printf "${BOLD_RED}(error)${RED} %s${RESET}\n" "$1"
-	fi
+    if [ "$2" == "1" ]; then
+        printf "        ${RED}%s${RESET}\n" "$1"
+    else
+        printf "${BOLD_RED}(error)${RED} %s${RESET}\n" "$1"
+    fi
 }
 
 fatal() {
-        if [ "$2" == "1" ]; then
-                printf "        ${RED}%s${RESET}\n" "$1"
-        else
-                printf "${BOLD_RED}(fatal)${RED} %s${RESET}\n" "$1"
-        fi
-
-	exit 1
+    if [ "$2" == "1" ]; then
+        printf "        ${RED}%s${RESET}\n" "$1"
+    else
+        printf "${BOLD_RED}(fatal)${RED} %s${RESET}\n" "$1"
+    fi
+    exit 1
 }
 
 warn() { printf "${BOLD_YELLOW}(warn)${YELLOW} %s${RESET}\n" "$1"; }
@@ -36,22 +35,22 @@ warn() { printf "${BOLD_YELLOW}(warn)${YELLOW} %s${RESET}\n" "$1"; }
 info() { printf "${BOLD_CYAN}(info)${CYAN} %s${RESET}\n" "$1"; }
 
 cd_fail() {
-	error "failed to change directory - line $(caller)"
-	exit 1
+    error "failed to change directory - line $(caller)"
+    exit 1
 }
 
 success() {
-	if [ "$2" == "1" ]; then
-		printf "          ${LIME}%s${RESET}\n" "$1"
-	else
-		printf "${BOLD_LIME}(success)${LIME} %s${RESET}\n" "$1"
-	fi
+    if [ "$2" == "1" ]; then
+        printf "          ${LIME}%s${RESET}\n" "$1"
+    else
+        printf "${BOLD_LIME}(success)${LIME} %s${RESET}\n" "$1"
+    fi
 }
 
 printf "${LIME}%s${RESET}\n" "MegaHack Installer for Linux"
 
 if [ "$DEBUG" == "1" ]; then
-   info "Debug logging enabled!"
+    info "Debug logging enabled!"
 fi
 
 # check for required packages
@@ -61,10 +60,10 @@ if ! hash xclip 2>/dev/null; then warn "xclip is not installed, you will have to
 
 echo
 if [ $missing_packages == true ] ; then
-   error "You are missing some programs."
-   error "Please install them using your distro's package manager to continue."
-   error "Additional information can be found above."
-   exit 0
+    error "You are missing some programs."
+    error "Please install them using your distro's package manager to continue."
+    error "Additional information can be found above."
+    exit 0
 fi
 
 # if fzf is installed, use it
@@ -89,43 +88,43 @@ fi
 
 echo
 if ! [ -f "$megahack_zip" ]; then
-   fatal "Could not find the file you specified!"
+    fatal "Could not find the file you specified!"
 fi
 
 megahack_zip=$(realpath "$megahack_zip")
 
 echo "Finding your steam path ..."
 if [ -f "$HOME/.steampid" ]; then
-   steam_pid=$(cat "$HOME/.steampid")
-   info "Steam PID: $steam_pid"
-   possible_path=$(readlink -e "/proc/$steam_pid/cwd")
+    steam_pid=$(cat "$HOME/.steampid")
+    info "Steam PID: $steam_pid"
+    possible_path=$(readlink -e "/proc/$steam_pid/cwd")
 fi
 
 if ! [ -d "$possible_path" ]; then
-   warn "Steam is not running, couldn't find directory from process"
-   warn "Searching manually, this can take a few seconds ..."
-   possible_path=$(find ~ -name 'steamapps' | grep -v compatdata | sed 's/steamapps//g')
+    warn "Steam is not running, couldn't find directory from process"
+    warn "Searching manually, this can take a few seconds ..."
+    possible_path=$(find ~ -name 'steamapps' | grep -v compatdata | sed 's/steamapps//g')
 fi
 
 function prompt_steam_path() {
-   echo
-   read -p "Please enter your Steam path: " in_path
-   steam_path="$in_path"
+    echo
+    read -p "Please enter your Steam path: " in_path
+    steam_path="$in_path"
 }
 
 steam_path=""
 if [ -n "$possible_path" ]; then
-   echo "Is this your Steam path?: $possible_path"
-   echo
-   read -p "[Y/n] :" answer
-   if [ "${answer,,}" == "y" ] || [ "${answer}" == "" ]; then
-      steam_path="$possible_path"
-   fi
-   if [ "${answer,,}" == "n" ]; then
-      prompt_steam_path
-   fi
+    echo "Is this your Steam path?: $possible_path"
+    echo
+    read -p "[Y/n] :" answer
+    if [ "${answer,,}" == "y" ] || [ "${answer}" == "" ]; then
+        steam_path="$possible_path"
+    fi
+    if [ "${answer,,}" == "n" ]; then
+        prompt_steam_path
+    fi
 else
-   prompt_steam_path
+    prompt_steam_path
 fi
 
 steam_path=${steam_path%/}
@@ -170,17 +169,17 @@ mkdir "/tmp/megahack" 2>/dev/null
 info "Extracting MegaHack Patcher ..."
 info "$megahack_zip"
 if [[ $megahack_zip == *.zip ]]; then
-   unzip "$megahack_zip" -d /tmp/megahack
+    unzip "$megahack_zip" -d /tmp/megahack
 else
-   fatal "unsupported file type - are you sure you're selecting a .zip file?"
+    fatal "unsupported file type - are you sure you're selecting a .zip file?"
 fi
 
 # find out where megahack is
 megahack_dir=$(ls /tmp/megahack)
 if [ "$DEBUG" == "1" ]; then
-   echo "-- contents of /tmp/megahack --"
-   echo "$megahack_dir"
-   echo "-- -- -- -- -- - -- -- -- -- --"
+    echo "-- contents of /tmp/megahack --"
+    echo "$megahack_dir"
+    echo "-- -- -- -- -- - -- -- -- -- --"
 fi
 
 megahack_dir="/tmp/megahack/$megahack_dir"
@@ -188,16 +187,16 @@ megahack_dir="/tmp/megahack/$megahack_dir"
 megahack_dir_contents=$(ls "$megahack_dir")
 
 if [ "$DEBUG" == "1" ]; then
-   echo "MegaHack Directory: $megahack_dir"
-   echo " -- Contents --"
-   echo "$megahack_dir_contents"
-   echo " -- -- -- -- --"
+    echo "MegaHack Directory: $megahack_dir"
+    echo " -- Contents --"
+    echo "$megahack_dir_contents"
+    echo " -- -- -- -- --"
 fi
 
 megahack_exe=$(echo "$megahack_dir_contents" | grep ".exe")
 
 if [ -z "$megahack_exe" ]; then
-	fatal "there's no executable in the provided zip file!"
+    fatal "there's no executable in the provided zip file!"
 fi
 
 success "Extracted MegaHack"
@@ -220,10 +219,10 @@ gd_exe_path="Z:${steam_path}/steamapps/common/Geometry Dash/GeometryDash.exe"
 info "Path to GD exe: ${gd_exe_path}"
 
 if hash xclip 2>/dev/null; then
-   echo "$gd_exe_path" | xclip -selection c
-   success "Copied path to clipboard!"
+    echo "$gd_exe_path" | xclip -selection c
+    success "Copied path to clipboard!"
 else
-   warn "xclip is not installed, please copy the path manually"
+    warn "xclip is not installed, please copy the path manually"
 fi
 echo
 
@@ -232,26 +231,26 @@ warn "use MHv6's libcurl.dll OR add 'WINEDLLOVERRIDES=\"Xinput9_1_0=n,b\" %comma
 warn "to Geometry Dash's start options in Steam OR MEGAHACK WON'T WORK!"
 warn "Do you wan't to use v6's libcurl.dll method?"
 read -p "[Y/n] :" answer_libcurl
-   if [ "${answer_libcurl,,}" == "y" ] || [ "${answer_libcurl}" == "" ]; then
-      use_v6_libcurl=1
+if [ "${answer_libcurl,,}" == "y" ] || [ "${answer_libcurl}" == "" ]; then
+    use_v6_libcurl=1
 fi
 
 if [ "$DEBUG" == "1" ]; then
-   echo "Starting MegaHack:"
-   echo "STEAM_COMPAT_DATA_PATH=\"${steam_path}/steamapps/compatdata/322170\" WINEPREFIX=\"$PWD\" \"${proton_dir}/proton\" runinprefix \"${megahack_dir}/${megahack_exe}\""
+    echo "Starting MegaHack:"
+    echo "STEAM_COMPAT_DATA_PATH=\"${steam_path}/steamapps/compatdata/322170\" WINEPREFIX=\"$PWD\" \"${proton_dir}/proton\" runinprefix \"${megahack_dir}/${megahack_exe}\""
 fi
 
 STEAM_COMPAT_DATA_PATH="${steam_path}/steamapps/compatdata/322170" WINEPREFIX="$PWD" "${proton_dir}/proton" runinprefix "${megahack_dir}/${megahack_exe}"
 
 if [ "$use_v6_libcurl" == "1" ]; then
-   warn "using v6's libcurl.dll to load!"
-   # this allows megahack v7 to load
-   cd "${steam_path}/steamapps/common/Geometry Dash"
-   rm libcurl.dll
-   info "Downloading v6 libcurl.dll"
-   wget -O "/tmp/megahack/libcurl.dll" "https://raw.githubusercontent.com/RoootTheFox/Linux-MegaHack-Installer/main/libcurl.dll"
-   cp "/tmp/megahack/libcurl.dll" .
-   mv hackproldr.dll absoluteldr.dll
+    warn "using v6's libcurl.dll to load!"
+    # this allows megahack v7 to load
+    cd "${steam_path}/steamapps/common/Geometry Dash"
+    rm libcurl.dll
+    info "Downloading v6 libcurl.dll"
+    wget -O "/tmp/megahack/libcurl.dll" "https://raw.githubusercontent.com/RoootTheFox/Linux-MegaHack-Installer/main/libcurl.dll"
+    cp "/tmp/megahack/libcurl.dll" .
+    mv hackproldr.dll absoluteldr.dll
 fi
 
 echo
